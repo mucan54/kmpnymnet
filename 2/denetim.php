95dd422596f56ae9096c4a36f7466da1287096de
mucan54 <mucan54@hotmail.com>
2017-04-24 08:58:14 +0300

    Update denetim.php

--- 2/denetim.php
+++ 2/denetim.php
@@ -6,11 +6,11 @@ ob_start();
 
 include("baglanti.php");
 
-$kullanici_adi = $_POST["kullanici_adi"];
-$parola = $_POST["parola"];
+//$kullanici_adi = $_POST["kullanici_adi"];
+//$parola = $_POST["parola"];
 
-//$kullanici_adi = htmlentities(mysql_real_escape_string($_POST["kullanici_adi"]));
-//$parola = md5(md5(htmlentities(mysql_real_escape_string($_POST["parola"]))));
+$kullanici_adi = htmlentities(mysql_real_escape_string($_POST["kullanici_adi"]));
+$parola = md5(md5(htmlentities(mysql_real_escape_string($_POST["parola"]))));
 
 $sorgula = mysql_query("SELECT * FROM uyeler WHERE kullanici_adi='{$kullanici_adi}' and parola='{$parola}'") or die (mysql_error());
 //$sorgula = mysql_query("SELECT * FROM uyeler WHERE kullanici_adi='$kullanici_adi' and parola='$parola'") or die (mysql_error());
