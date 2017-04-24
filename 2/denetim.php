ca0df303843b8a09a2d32276729e617df1c26643
mucan54 <mucan54@hotmail.com>
2017-04-24 08:51:31 +0300

    Update denetim.php

--- 2/denetim.php
+++ 2/denetim.php
@@ -6,11 +6,11 @@ ob_start();
 
 include("baglanti.php");
 
-$kullanici_adi = $_POST["kullanici_adi"];
-$parola = $_POST["parola"];
+//$kullanici_adi = $_POST["kullanici_adi"];
+//$parola = $_POST["parola"];
 
-//$kullanici_adi = htmlentities(mysql_real_escape_string($_POST["kullanici_adi"]));
-//$parola = md5(md5(htmlentities(mysql_real_escape_string($_POST["parola"]))));
+$kullanici_adi = htmlentities(mysql_real_escape_string($_POST["kullanici_adi"]));
+$parola = md5(md5(htmlentities(mysql_real_escape_string($_POST["parola"]))));
 
 //$sorgula = mysql_query("SELECT * FROM uyeler WHERE kullanici_adi='{$kullanici_adi}' and parola='{$parola}'") or die (mysql_error());
 $sorgula = mysql_query("SELECT * FROM uyeler WHERE kullanici_adi='$kullanici_adi' and parola='$parola'") or die (mysql_error());
@@ -25,14 +25,14 @@ setcookie("kullanici_adi",$kullanici_adi,time()+60*60*24);
 setcookie("parola",$parola,time()+60*60*24);
 
 echo "true";
-//echo str_repeat("<br>", 8)."<center><img src=images/yukleniyor.gif border=0 /> Giriþ baþarýlý, lütfen bekleyiniz..</center>";
+//echo str_repeat("<br>", 8)."<center><img src=images/yukleniyor.gif border=0 /> GiriÃ¾ baÃ¾arÃ½lÃ½, lÃ¼tfen bekleyiniz..</center>";
 //header("Refresh: 2; url=anasayfa.php");
 }
 
 else
 {
 		
-echo str_repeat("<br>", 8)."<center><img src=images/hata.gif border=0 /> Kullanýcý adý veya parola hatalý!</center>";
+echo str_repeat("<br>", 8)."<center><img src=images/hata.gif border=0 /> KullanÃ½cÃ½ adÃ½ veya parola hatalÃ½!</center>";
 header("Refresh: 2; url=index.php");
 	
 }
